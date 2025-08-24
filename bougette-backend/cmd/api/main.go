f13c721e1467708281ae4ba485a75628624de885
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2025-08-24 20:55:35 -0300

    feat: loading database on main

--- bougette-backend/cmd/api/main.go
+++ bougette-backend/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bougette-backend/cmd/api/handlers"
+	"bougette-backend/common"
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
@@ -9,17 +11,36 @@ import (
 	"os"
 )
 
+type Application struct {
+	logger  echo.Logger
+	server  *echo.Echo
+	handler handlers.Handler
+}
+
 func main() {
 	e := echo.New()
-
-	err := godotenv.Load()
+	errenv := godotenv.Load()
+	db, err := common.NewMysql()
 	if err != nil {
+		log.Fatal(err.Error())
+	}
+	if errenv != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	h := handlers.Handler{
+		DB: db,
+	}
+
+	app := Application{
+		logger:  e.Logger,
+		server:  e,
+		handler: h,
+	}
+	fmt.Println(app)
 	port := os.Getenv("APP_PORT")
 	appAdress := fmt.Sprintf("localhost:%s", port)
 	e.Logger.Fatal(e.Start(appAdress))
