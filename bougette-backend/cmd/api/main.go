24f3b117d47d762651291986f44e1816e246e0c6
JoseHenriquePatrocinio <henriquepatrocinio98@gmail.com>
2025-08-23 22:52:20 -0300

    conf: setando valores do .env

--- bougette-backend/cmd/api/main.go
+++ bougette-backend/cmd/api/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	e := echo.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	port := os.Getenv("APP_PORT")
+	appAdress := fmt.Sprintf("localhost:%s", port)
+	e.Logger.Fatal(e.Start(appAdress))
 }
